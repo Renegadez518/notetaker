77af9e700119e94c9b0dbbfb904c8140d92c8da4
Renegadez518 <49694652+Renegadez518@users.noreply.github.com>
2023-05-28 23:59:26 +0530

    changes

--- login.js
+++ login.js
@@ -12,4 +12,16 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
       alert('Invalid username or password!');
     }
   });
+  function togglePassword() {
+    var passwordField = document.getElementById("password");
+    var toggleButton = document.querySelector(".toggle-password");
   
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      toggleButton.textContent = "Hide";
+    } 
+    else {
+      passwordField.type = "password";
+      toggleButton.textContent = "Show";
+    }
+  }
